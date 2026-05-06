2fccd5a187d2574183f3b457e2a46834a88e64b6
ARAK BALINESIA <brkkids@gmail.com>
2026-05-06 23:06:50 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,9 +1,20 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "shincore_db");
+if (isset($_ENV['MYSQLHOST'])) {
+  // Railway
+  $conn = new mysqli(
+    $_ENV['MYSQLHOST'],
+    $_ENV['MYSQLUSER'],
+    $_ENV['MYSQLPASSWORD'],
+    $_ENV['MYSQLDATABASE']
+  );
+} else {
+  // Localhost
+  $conn = new mysqli("localhost", "root", "", "shincore_db");
+}
 
-if (!$conn) {
-    die("Koneksi database gagal!");
+if ($conn->connect_error) {
+  die("Koneksi gagal: " . $conn->connect_error);
 }
 
 ?>
--- order.php
+++ order.php
@@ -1,7 +1,16 @@
 <?php
-include "config.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-$serverKey = "Mid-server-W9fX-JUgksLia453D7FiqaT6";
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  echo json_encode([
+    "status" => "error",
+    "message" => "Akses tidak valid"
+  ]);
+  exit;
+}
+
+include "config.php";
 
 $game      = $_POST['game'];
 $player_id = $_POST['player_id'];
@@ -19,60 +28,24 @@ $price = $priceList[$diamond] ?? 0;
 
 $order_id = "ORDER-" . time() . rand(100,999);
 
-/* 1. SIMPAN ORDER KE DATABASE */
-mysqli_query($conn, "
+/* SIMPAN KE DATABASE (TANPA MIDTRANS) */
+$query = mysqli_query($conn, "
 INSERT INTO orders 
 (order_id, game, player_id, diamond, payment, price, status)
 VALUES 
-('$order_id','$game','$player_id','$diamond','$payment','$price','pending')
+('$order_id','$game','$player_id','$diamond','$payment','$price','Pending')
 ");
 
-/* 2. SET PAYMENT TYPE */
-if($payment == "Bank"){
-    $payment_type = "bank_transfer";
-    $bank = "bca";
-}
-
-/* 3. REQUEST KE MIDTRANS CORE API */
-$data = [
-  "payment_type" => $payment_type,
-  "transaction_details" => [
-    "order_id" => $order_id,
-    "gross_amount" => (int)$price
-  ],
-  "bank_transfer" => [
-    "bank" => $bank
-  ]
-];
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.midtrans.com/v2/charge");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-$headers = [
-  "Content-Type: application/json",
-  "Authorization: Basic " . base64_encode($serverKey . ":")
-];
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-
-if(curl_errno($ch)){
-    header('Content-Type: application/json');
+if($query){
+    echo json_encode([
+        "status" => "success",
+        "message" => "Order berhasil dibuat",
+        "order_id" => $order_id
+    ]);
+}else{
     echo json_encode([
         "status" => "error",
-        "message" => curl_error($ch)
+        "message" => "Gagal simpan ke database"
     ]);
-    exit;
 }
-
-curl_close($ch);
-
-/* kirim ke frontend sebagai JSON valid */
-header('Content-Type: application/json');
-echo $result;
 ?>
