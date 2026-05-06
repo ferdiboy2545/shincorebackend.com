5d2aea3dbaf0e690f4f52088b82195f396efcfb4
ARAK BALINESIA <brkkids@gmail.com>
2026-05-06 23:37:35 +0700

    Add files via upload

--- midtrans-notif.php
+++ midtrans-notif.php
@@ -1,30 +1,3 @@
 <?php
-include "config.php";
-
-$json = file_get_contents("php://input");
-$data = json_decode($json, true);
-
-$order_id = $data['order_id'];
-$status = $data['transaction_status'];
-$midtrans_id = $data['transaction_id'];
-
-if($status == "settlement"){
-
-    mysqli_query($conn, "
-    UPDATE orders 
-    SET status='success',
-    midtrans_transaction_id='$midtrans_id'
-    WHERE order_id='$order_id'
-    ");
-
-} else {
-
-    mysqli_query($conn, "
-    UPDATE orders 
-    SET status='$status',
-    midtrans_transaction_id='$midtrans_id'
-    WHERE order_id='$order_id'
-    ");
-
-}
+exit;
 ?>
